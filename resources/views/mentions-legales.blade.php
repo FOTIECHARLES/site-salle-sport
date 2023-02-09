f8880978e9a5f1074d10871c5365d23524b28e28
fotiecharles <sopcharles1@gmail.com>
2023-02-10 00:32:12 +0100

    Modification de la page mentions legales:
    Début de redaction du document README.md

--- resources/views/mentions-legales.blade.php
+++ resources/views/mentions-legales.blade.php
@@ -9,9 +9,8 @@
 
 @section('content')
     <h2>Mentions-Legales</h2>
-    
-    
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, voluptatem.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam at eum aliquid atque reprehenderit quia incidunt a temporibus nemo, nesciunt eius error earum quo minima laborum unde quasi. Ipsum, hic.</p><br/>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam at eum aliquid atque reprehenderit quia incidunt a temporibus nemo, nesciunt eius error earum quo minima laborum unde quasi.</p>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore veniam explicabo inventore saepe sit tempore sapiente id repudiandae cum delectus!</p>
+
 @endsection
